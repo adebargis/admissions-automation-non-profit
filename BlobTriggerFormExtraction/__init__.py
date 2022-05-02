d8031b8bfdef7a862f685e2e4c2db602c45dd239
Alexandre Debargis <adebargis@microsoft.com>
2022-05-02 17:07:16 -0400

    updated Python code to support CosmosDB output, still need to add Cosmos in IaC + App settings for Function App

--- BlobTriggerFormExtraction/__init__.py
+++ BlobTriggerFormExtraction/__init__.py
@@ -1,12 +1,16 @@
+import uuid
 import logging
+from turtle import done
 import azure.functions as func
 import json
 import time
 import os
 from azure.ai.formrecognizer import DocumentAnalysisClient
 from azure.core.credentials import AzureKeyCredential
+from azure.cosmos import CosmosClient
+from azure.cosmos.partition_key import PartitionKey
 
-def main(myblob: func.InputStream):
+def main(myblob: func.InputStream) -> func.HttpResponse:
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {myblob.name}\n"
                  f"Blob Size: {myblob.length} bytes\n"
@@ -23,6 +27,8 @@ def main(myblob: func.InputStream):
     
     endpoint = os.environ["AZURE_FORM_RECOGNIZER_ENDPOINT"]
     key = os.environ["AZURE_FORM_RECOGNIZER_KEY"]
+    cosmos_url = os.environ["COSMOS_ENDPOINT"]
+    cosmos_key = os.environ["COSMOS_KEY"]
 
     # sample document
     #docUrl = myblob.uri #"https://testadmissions.blob.core.windows.net/test/alex_form_en.jpg"
@@ -40,7 +46,10 @@ def main(myblob: func.InputStream):
     #        "prebuilt-document", docUrl)
     result = poller.result()
     #print(result)
+    #print(result.key_value_pairs)
 
+    keys = []
+    values = []
     print("----Key-value pairs found in document----")
     for kv_pair in result.key_value_pairs:
         if kv_pair.key:
@@ -49,15 +58,60 @@ def main(myblob: func.InputStream):
                         kv_pair.key.content,
                     )
                 )
+            key = kv_pair.key.content
+        if not kv_pair.value:
+            value="NA"
         if kv_pair.value:
             print(
                     "Value '{}'\n".format(
                         kv_pair.value.content,
                     )
                 )
+            value = kv_pair.value.content
+        keys.append(key)
+        values.append(value)
+
+        #admissions_json.append(admission_json)
+    admissions_json = {}
+    #print(keys)
+    #print(values)
+    mapping = dict(zip(keys,values))
+    mapping['id'] = str(uuid.uuid4())
+    #print(mapping)
+    json_object = json.dumps(mapping, indent = 4) 
+    #print(json_object)
+    #for i in keys, values:
+    #    key = keys[i]
+    #    value = values[i]
+    #    admissions_json[key] = value
+   
 
     print("----------------------------------------")
+    client = CosmosClient(cosmos_url, cosmos_key)
+    database_name = 'testcsa'
+    database = client.create_database_if_not_exists(id=database_name)
+    container_name = 'items'
+    container = database.create_container_if_not_exists(
+        id=container_name,
+        partition_key=PartitionKey(path="/Date"), 
+        offer_throughput=400
+    )
+    container.create_item(body=mapping)
+    #doc.set(mapping)
+    
+    
 
+    #print(admissions_json)
+    #admissions_json = []
+    #for kv_pair in result.key_value_pairs:
+    #    admission_json = {
+    #       '{}'.format(kv_pair.key.content) : '{}'.format(kv_pair.value.content)
+    #    }
+    #    admissions_json.append(admission_json)
+    
+    
+
+ 
     #analyze_general_documents()
 
 
